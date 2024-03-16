7ba5921045f5f82356af8009c75ebc059e9f4e35
Paul Braetz <paul.braetz@rhomicro.com>
2024-03-16 02:07:39 +0100

    add css settings composer

--- Composition.Presentation.Views.Blazor/Composers.cs
+++ Composition.Presentation.Views.Blazor/Composers.cs
@@ -1,9 +1,11 @@
 ﻿namespace RhoMicro.ApplicationFramework.Composition.Presentation.Views.Blazor;
 
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
 using RhoMicro.ApplicationFramework.Presentation.Views.Blazor;
 using RhoMicro.ApplicationFramework.Presentation.Views.Blazor.Abstractions;
-
-using SimpleInjector;
 
 /// <summary>
 /// Contains composers able to compose blazor view object graphs.
@@ -11,7 +13,28 @@ using SimpleInjector;
 public static class Composers
 {
     /// <summary>
-    /// Gets the default composer instance.
+    /// Gets the styles composer instance.
     /// </summary>
-    public static IComposer Default { get; } = Composer.Create(c => c.RegisterConditional<ICssStyle, CssStyle>(Lifestyle.Singleton, ctx => !ctx.Handled));
+    public static IComposer CreateStylesComposer(IServiceCollection services, IConfiguration configuration) =>
+        Composer.Create(c =>
+        {
+            var cssSettingsSection = configuration.GetSection("CssStyleSettings");
+
+            var settingsTypes = typeof(CssStyleSettings).Assembly
+                .GetTypes()
+                .Where(t => t.Name.EndsWith("Settings", StringComparison.Ordinal) && t.IsAssignableTo(typeof(ICssStyle)));
+
+            foreach(var settingsType in settingsTypes)
+            {
+                var optionsImplType = typeof(ConfigureNamedOptions<>).MakeGenericType(settingsType);
+                var optionsServiceType = typeof(IOptions<>).MakeGenericType(settingsType);
+
+                var cssStyleName = settingsType.Name[..^( "Settings".Length + 1 )];
+
+                var instance = optionsImplType
+                    .GetConstructor([typeof(String), typeof(Action<>).MakeGenericType(settingsType)])
+                    ?.Invoke(null, [null, (Object settings) => cssSettingsSection.GetSection(cssStyleName).Bind(settings)])
+                    ?? throw new InvalidOperationException($"Unable to construct options: {settingsType}");
+            }
+        });
 }
