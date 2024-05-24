743f652be1278b045e7dd41f992d8dbc252e16a2
Paul Brätz <paul.braetz@rhomicro.com>
2024-05-24 21:22:10 +0200

    add result based service request

--- Common/Abstractions/IServiceRequest.cs
+++ Common/Abstractions/IServiceRequest.cs
@@ -11,3 +11,10 @@ public interface IServiceRequest<TResult>
     /// </summary>
     CancellationToken CancellationToken { get; }
 }
+
+/// <summary>
+/// Represents a request parameter object whose execution must result in a <see cref="Result{TSuccess, TFailure}"/>.
+/// </summary>
+/// <typeparam name="TSuccess">The type of success to produce.</typeparam>
+/// <typeparam name="TFailure">The type of failure to produce.</typeparam>
+public interface IServiceRequest<TSuccess, TFailure> : IServiceRequest<Result<TSuccess, TFailure>>;
