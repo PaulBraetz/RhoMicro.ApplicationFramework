8a70a989258bb797bb0f9b29564fcc98910e114c
Paul Brätz <paul.braetz@rhomicro.com>
2024-05-24 18:38:17 +0200

    add generic result

---
+++ Common/Result.cs
@@ -0,0 +1,44 @@
+﻿namespace RhoMicro.ApplicationFramework.Common;
+
+using RhoMicro.CodeAnalysis;
+
+/// <summary>
+/// Represents a generic success result.
+/// </summary>
+public readonly struct Success;
+/// <summary>
+/// Represents a generic failure result.
+/// </summary>
+public readonly struct Failure;
+/// <summary>
+/// Represents a generic result capable of representing either a success or a failure.
+/// </summary>
+/// <typeparam name="TSuccess">The type of success represented.</typeparam>
+/// <typeparam name="TFailure">The type of failure represented.</typeparam>
+public readonly partial struct Result<[UnionType(Alias = "Success")] TSuccess, [UnionType(Alias = "Failure")] TFailure>;
+/// <summary>
+/// Provides static factory methods for the <see cref="Result{TSuccess, TFailure}"/> type.
+/// </summary>
+public static class Result
+{
+    /// <summary>
+    /// Creates a new generic success result.
+    /// </summary>
+    /// <returns>A new generic success result.</returns>
+    public static Result<Success, Failure> CreateSuccess() => new Success();
+    /// <summary>
+    /// Creates a new generic success result.
+    /// </summary>
+    /// <returns>A new generic success result.</returns>
+    public static Result<Success, TFailure> CreateSuccess<TFailure>() => new Success();
+    /// <summary>
+    /// Creates a new generic failure result.
+    /// </summary>
+    /// <returns>A new generic failure result.</returns>
+    public static Result<Success, Failure> CreateFailure() => new Failure();
+    /// <summary>
+    /// Creates a new generic failure result.
+    /// </summary>
+    /// <returns>A new generic failure result.</returns>
+    public static Result<TSuccess, Failure> CreateFailure<TSuccess>() => new Failure();
+}
