0892f79ef7cdd801df7c2f51d59cad3bde0512ec
Paul Brätz <paul.braetz@rhomicro.com>
2024-07-11 18:23:11 +0200

    move tolower request to dedicated file

---
+++ Apps/TestApp/TA.Presentation.Views.Blazor/ToLower.cs
@@ -0,0 +1,21 @@
+﻿#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+namespace TA.Presentation.Views.Blazor;
+
+using System;
+
+using RhoMicro.ApplicationFramework.Common.Abstractions;
+
+public partial record struct ToLower :
+    IApiRequest<ToLower, ToLower.Result, ToLower, ToLower.Result>,
+    IApiRequestDto<ToLower, ToLower.Result>
+{
+    public readonly partial record struct Result(String Value)
+        : IApiResult<Result, Result>,
+        IApiResultDto<Result>
+    {
+        public Result ToResult() => this;
+        public Result ToDto() => this;
+    }
+    public ToLower ToDto() => this;
+    public ToLower ToRequest() => this;
+}
--- Apps/TestApp/TA.Presentation.Views.Blazor/ToLowerService.cs
+++ Apps/TestApp/TA.Presentation.Views.Blazor/ToLowerService.cs
@@ -4,22 +4,6 @@ namespace TA.Presentation.Views.Blazor;
 using System;
 
 using RhoMicro.ApplicationFramework.Aspects;
-using RhoMicro.ApplicationFramework.Common.Abstractions;
-
-public partial record struct ToLower :
-    IApiRequest<ToLower, ToLower.Result, ToLower, ToLower.Result>,
-    IApiRequestDto<ToLower, ToLower.Result>
-{
-    public readonly partial record struct Result(String Value)
-        : IApiResult<Result, Result>,
-        IApiResultDto<Result>
-    {
-        public Result ToResult() => this;
-        public Result ToDto() => this;
-    }
-    public ToLower ToDto() => this;
-    public ToLower ToRequest() => this;
-}
 
 public sealed partial class ToLowerService
 {
