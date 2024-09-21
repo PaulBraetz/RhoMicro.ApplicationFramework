8673224d7f6364b90985928622ca211977859dbf
Paul Brätz <paul.braetz@rhomicro.com>
2024-09-21 04:12:09 +0200

    add interceptor types

---
+++ Common/Abstractions/IInterceptor.cs
@@ -0,0 +1,23 @@
+﻿namespace RhoMicro.ApplicationFramework.Common.Abstractions;
+
+/// <summary>
+/// Intercepts arbitrary objects.
+/// </summary>
+/// <typeparam name="T">
+/// The type of object to intercept.
+/// </typeparam>
+public interface IInterceptor<T>
+{
+    /// <summary>
+    /// Intercepts an object.
+    /// </summary>
+    /// <param name="obj">The object to intercept.</param>
+    /// <param name="cancellationToken">
+    /// The cancellation token used to interception to be cancelled.
+    /// </param>
+    /// <returns>
+    /// A task that, upon completion, will contain the (potentially) transformed object resulting from the
+    /// interception.
+    /// </returns>
+    ValueTask<T> Intercept(T obj, CancellationToken cancellationToken);
+}
---
+++ Common/AggregateInterceptor.cs
@@ -0,0 +1,29 @@
+﻿namespace RhoMicro.ApplicationFramework.Common;
+
+using RhoMicro.ApplicationFramework.Common.Abstractions;
+
+/// <summary>
+/// Aggregates interceptors for collection injection.
+/// </summary>
+/// <typeparam name="T">
+/// The type of object to intercept.
+/// </typeparam>
+/// <param name="interceptors">
+/// The interceptor collection to wrap.
+/// </param>
+public sealed class AggregateInterceptor<T>(
+    IEnumerable<IInterceptor<T>> interceptors) :
+    IInterceptor<T>
+{
+    //eagerly enumerate as simpleinjector injects an enumerable that always 
+    //yields from the container resolve
+    private readonly IEnumerable<IInterceptor<T>> _interceptors = interceptors.ToList();
+    /// <inheritdoc/>
+    public async ValueTask<T> Intercept(T obj, CancellationToken cancellationToken)
+    {
+        foreach(var interceptor in _interceptors)
+            obj = await interceptor.Intercept(obj, cancellationToken).ConfigureAwait(false);
+
+        return obj;
+    }
+}
